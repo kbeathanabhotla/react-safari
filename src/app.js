424b309801eff32500cf0f28f38ab1c1750f661c
Sai Krishna Kishore <beathanabhotla.kishore@gmail.com>
2016-03-02 17:34:39 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar';
+import Page from './components/Page';
 
 class App extends React.Component {
   render() {
-    return <NavBar userName="Sai" />;
+    return (
+      <div id="content">
+        <NavBar userName="Sai" />
+        <Page>
+          <h1>
+            Hello World!
+          </h1>
+        </Page>
+      </div>
+    );
   }
 }
 
--- src/components/Page.js
+++ src/components/Page.js
@@ -3,6 +3,9 @@ import React from 'react';
 class Page extends React.Component {
   render() {
     return (
+      <div className="container">
+        {this.props.children}
+      </div>
     );
   }
 }
